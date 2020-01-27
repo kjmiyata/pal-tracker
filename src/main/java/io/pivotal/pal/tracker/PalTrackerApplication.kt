d93c70a3cac3b1ab17ccfde54f2e55cb844c5313
kjmiyata <kevinmiyata@discover.com>
2020-01-27 11:27:56 -0600

    Create Spring app

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.pal.tracker
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class PalTrackerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(PalTrackerApplication::class.java, *args)
+}
---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.kt
@@ -0,0 +1,13 @@
+package io.pivotal.pal.tracker
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+open class WelcomeController {
+
+    @GetMapping("/")
+    fun sayHello(): String {
+        return "hello"
+    }
+}
