183177d0e91e00fdf72d89eb6888c5c05a3ed7de
kjmiyata <kevinmiyata@discover.com>
2020-01-27 11:44:15 -0600

    simplify hello method

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.kt
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.kt
@@ -7,7 +7,5 @@ import org.springframework.web.bind.annotation.RestController
 open class WelcomeController {
 
     @GetMapping("/")
-    fun sayHello(): String {
-        return "hello"
-    }
+    fun sayHello(): String = "hello"
 }
