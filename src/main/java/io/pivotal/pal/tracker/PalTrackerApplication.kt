14c1190155fa80b63a0e3f411a9f297dc826c046
Kevin Miyata <42757205+kjmiyata@users.noreply.github.com>
2020-01-29 14:21:36 -0600

    Add JDBC and SQL support for controllers (#4)
    * fix time entry controller constructor
    
    * Add tests for JDBC lab
    
    * add JDBC config and tests
    
    * update tests
    
    * clean up
    
    Co-authored-by: Bill Kable <bkable@pivotal.io>

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
@@ -3,13 +3,14 @@ package io.pivotal.pal.tracker
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @SpringBootApplication
 open class PalTrackerApplication {
 
     @Bean
-    open fun repo(): TimeEntryRepository {
-        return InMemoryTimeEntryRepository()
+    open fun repo(dataSource: DataSource): TimeEntryRepository {
+        return JdbcTimeEntryRepository(dataSource)
     }
 }
 
