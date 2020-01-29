1d9248964d827fa9be94e7c5dc3929628ae1fcd3
Kevin Miyata <42757205+kjmiyata@users.noreply.github.com>
2020-01-29 11:38:22 -0600

    Add database files (#3)
    * database stuff
    
    * fix
    
    * add database files
    
    Co-authored-by: Bill Kable <bkable@pivotal.io>

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.kt
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.kt
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class TimeEntryController(@Autowired var repository: TimeEntryRepository) {
+class TimeEntryController(var repository: TimeEntryRepository) {
 
     @PostMapping(value = ["/time-entries"])
     fun create(@RequestBody entry: TimeEntry): ResponseEntity<*> {
