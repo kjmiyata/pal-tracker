c22bb840570f022db8b48b6e15eec6549e7ea0b5
kjmiyata <kevinmiyata@discover.com>
2020-01-29 14:13:20 -0600

    update tests

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
@@ -3,19 +3,14 @@ package io.pivotal.pal.tracker
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.jdbc.core.JdbcTemplate
 import javax.sql.DataSource
 
 @SpringBootApplication
 open class PalTrackerApplication {
 
-    open fun dataSource(): JdbcTemplate {
-        return JdbcTemplate()
-    }
-
     @Bean
-    open fun repo(): TimeEntryRepository {
-        return InMemoryTimeEntryRepository()
+    open fun repo(dataSource: DataSource): TimeEntryRepository {
+        return JdbcTimeEntryRepository(dataSource)
     }
 }
 
