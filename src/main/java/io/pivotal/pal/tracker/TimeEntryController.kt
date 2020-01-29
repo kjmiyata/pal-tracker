86847471107b8b117da9fd05b035fffb53211689
kjmiyata <kevinmiyata@discover.com>
2020-01-29 13:15:11 -0600

    fix time entry controller constructor

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.kt
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.kt
@@ -1,12 +1,11 @@
 package io.pivotal.pal.tracker
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class TimeEntryController(var repository: TimeEntryRepository) {
+class TimeEntryController(private val repository: TimeEntryRepository) {
 
     @PostMapping(value = ["/time-entries"])
     fun create(@RequestBody entry: TimeEntry): ResponseEntity<*> {
